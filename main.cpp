0a98a2b9922529c3a3ee9ab2ecdef6c1456ff4ee
Hasan Darwish <hasan.darwish.work@gmail.com>
2026-08-12 22:25:11 +0300

    Improved the number validation function

--- main.cpp
+++ main.cpp
@@ -2,11 +2,35 @@
 #include <string>
 
 bool isNumberValid(const std::string &str) {
-  return std::all_of(str.begin(), str.end(), [](unsigned char c){
+  bool has_only_digits_and_dots = std::all_of(str.begin(), str.end(), [](unsigned char c){
     return std::isdigit(c) || c == '.';
   });
+
+  if(!has_only_digits_and_dots) {return false;}
+
+  int number_of_dots = 0;
+  for(char c:str) {
+    if (c == '.') {number_of_dots++;}
+  }
+  if(number_of_dots > 1) {return false;}
+
+  if (str[0] == '.' || str[str.length() - 1] == '0') {return false;}
+  return true;
 }
 
 int main() {
-  return 1;
+  std::cout << "Hello and welcome to the square rooter algorithm!" << std::endl;
+  std::cout << "An algorithm completely built with native C++ (No external dependencies)" << std::endl;
+  std::cout << "-----------\n\n";
+  std::cout << "Please enter the number you want to square root:\n";
+
+  std::string input;
+  std::cin >> input;
+
+  if(!isNumberValid(input)) {
+    std::cout << "Error! the provided number is not a number. Please provide a real number!\n";
+    return 1;
+  }
+  
+  return 0;
 }
