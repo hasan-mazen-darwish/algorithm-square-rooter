8631c384fd93b2263dbf91192f4ecc86afdad952
Hasan Darwish <hasan.darwish.work@gmail.com>
2026-08-13 07:15:04 +0300

    Added the function that dissects the string into usable vectors

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 bool isNumberValid(const std::string &str) {
   bool has_only_digits_and_dots = std::all_of(str.begin(), str.end(), [](unsigned char c){
@@ -14,8 +15,67 @@ bool isNumberValid(const std::string &str) {
     if(number_of_dots > 1) {return false;}
   }
 
-  if (str[0] == '.' || str[str.length() - 1] == '0') {return false;}
+  bool hasDotInTheBeginning = str[0] == '.';
+  bool hasDotInTheEnd = str[str.length() - 1] == '.';
+
+  if (hasDotInTheBeginning || hasDotInTheEnd) {return false;}
   return true;
+}
+
+struct Parts {
+  std::vector<uint> whole;
+  std::vector<uint> decimal;
+};
+
+Parts dissectStringNumber(const std::string &input) {
+  Parts parts = {{}, {}};
+
+  size_t dotIndex = input.find('.');
+  std::string wholeString = input;
+  std::string decimalString = "";
+  if(dotIndex != std::string::npos) {
+    wholeString = input.substr(0, dotIndex);
+    decimalString = input.substr(dotIndex + 1);
+  }
+
+  for (int i=0; i<wholeString.length() ; i++) {
+    if(wholeString[i] != '0') break;
+    else wholeString.pop_back();
+  }
+  if(wholeString == "") wholeString = "00";
+
+  for (int i=decimalString.length()-1 ; i>=0 ; i--) {
+    if(decimalString[i] != '0') break;
+    else decimalString.pop_back();
+  }
+
+  // Here I am checking if the whole numbers are pairs like 1234 or not like 12345
+  bool isWholePartPairs = wholeString.length()%2 == 0;
+  // Same for the decimals
+  bool isDecimalPartPairs = decimalString.length()%2 == 0;
+
+  if(!isWholePartPairs) {
+    parts.whole.push_back(wholeString[0] - '0');
+    wholeString = wholeString.substr(1, wholeString.length()-2);
+  }
+  for(int i=0 ; i<wholeString.length() ; i+=2) {
+    parts.whole.push_back(std::stoi(wholeString.substr(i, 2)));
+  }
+
+  if(decimalString == "") return parts;
+
+  for(int i=0 ; i<decimalString.length()-1 ; i+=2) {
+    parts.decimal.push_back(std::stoi(decimalString.substr(i, 2)));
+  }
+
+  if(!isDecimalPartPairs) {
+    parts.decimal.push_back(
+          10*(decimalString[decimalString.length()-1]-'0')
+        );
+    decimalString.pop_back();
+  }
+  
+  return parts;
 }
 
 int main() {
@@ -44,6 +104,10 @@ int main() {
   }
 
   std::cout << "\nGot it. So, you're approximating the square root of the number " << input << " to " << digits << " digits.\n";
+
+  Parts parts = dissectStringNumber(input);
+  std::vector<uint> whole = parts.whole;
+  std::vector<uint> decimal = parts.decimal;
   
   return 0;
 }
